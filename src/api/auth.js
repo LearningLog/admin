86ec109033175bb07feb0fe524583c02f7ff7ec6
yanhuakang <LearningLog@126.com>
2018-12-07 22:49:25 +0800

    first commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -71,6 +71,7 @@ export function removeUserInfo () {
  * @return {string} 用户的 Token 身份令牌
  */
 export function getToken () {
+  debugger
   try {
     // 本地存储中的 user-info 可能不是一个有效的 JSON 格式字符串
     // 所以我们这里为了避免程序出错，使用了 try-catcher 来捕获转换失败的异常
